d769d451c468b4f4cee1eb2cd3fd71354f328daf
hernan <hernan@Ubuntu.myguest.virtualbox.org>
2023-11-29 10:47:49 -0300

    updating-users-2

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 	validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
 	has_secure_password
-	validates :password, presence: true, length: { minimum: 6 }
+	validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
 	# Returns the hash digest of the given string.
 	def User.digest(string)
@@ -23,6 +23,13 @@ class User < ApplicationRecord
 	def remember
 		self.remember_token = User.new_token
 		update_attribute(:remember_digest, User.digest(remember_token))
+		remember_digest
+	end
+
+	# Returns a session token to prevent session hijacking.
+	# We reuse the remember digest for convenience.
+	def session_token
+		remember_digest || remember
 	end
 
 	# Returns true if the given token matches the digest.
